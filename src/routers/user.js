7fb23f57a4319e9b20b46d54b9c755cb611a1b8e
heretodesign <senwelo.works@gmail.com>
2020-09-24 11:55:13 +0800

    auth user login using jwt

--- src/routers/user.js
+++ src/routers/user.js
@@ -14,10 +14,9 @@ router.post('/users', async (req, res) => {
 
 router.post('/users/login', async (req, res) => {
     try {
-        console.log("inside ready!")
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        console.log("inside!" + user)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
 
     } catch (error) {
         res.status(400).send(error)
